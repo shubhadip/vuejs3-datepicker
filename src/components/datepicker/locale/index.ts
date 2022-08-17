24237a2a262493e6da6e042ce2ed817ddabec800
Frank Giger <frank.giger@mfgroup.ch>
2022-08-17 15:38:52 +0200

    Add italian language

--- src/components/datepicker/locale/index.ts
+++ src/components/datepicker/locale/index.ts
@@ -283,6 +283,41 @@ const pt = (): ILocale => {
   };
 };
 
+const it = (): ILocale => {
+  const langName = 'Italian';
+  const monthFullName = [
+    'Gennaio',
+    'Febbraio',
+    'Marzo',
+    'Aprile',
+    'Maggio',
+    'Giugno',
+    'Luglio',
+    'Agosto',
+    'Settembre',
+    'Ottobre',
+    'Novembre',
+    'Dicembre',
+  ];
+  const shortName = ['Gen', 'Feb', 'Mar', 'Apr', 'Mag', 'Giu', 'Lug', 'Ago', 'Set', 'Ott', 'Nov', 'Dic'];
+  const days = ['Dom', 'Lun', 'Mar', 'Mer', 'Gio', 'Ven', 'Sab'];
+  const daysNames = ['Domenica', 'Lunedì', 'Martedì', 'Mercoledì', 'Gioved', 'Venerdì', 'Sabato'];
+  const rtl = false;
+  const ymd = false;
+  const yearSuffix = '';
+  return {
+    months: monthFullName,
+    monthsAbbr: shortName,
+    days,
+    language: langName,
+    yearSuffix,
+    ymd,
+    rtl,
+    langName,
+    daysNames,
+  };
+};
+
 export const data = {
   af: af(),
   hi: hi(),
@@ -292,4 +327,5 @@ export const data = {
   fr: fr(),
   nl: nl(),
   pt: pt(),
+  it: it(),
 };
